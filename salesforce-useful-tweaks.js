a4611aa0a8fdaa02952852d66b1f235fc3ef3c1e
David A. Desrosiers <setuid@gmail.com>
2018-10-26 10:29:50 -0400

    Added support for linking in CVEs, based on a case that came in this morning.

--- salesforce-useful-tweaks.js
+++ salesforce-useful-tweaks.js
@@ -4,8 +4,15 @@
 // @description    Style and tweak Salesforce to be more productive for Engineers and Support
 // @include        /^https?://.*.salesforce\.com/.*$/
 // @author         setuid@gmail.com
-// @version        1.7
+// @version        1.8
 // ==/UserScript==
+
+// e1, e1     == element 1, 2
+// els1, els2 == element selector 1, 2
+
+var goog_search = "http://www.google.com/search?q=";
+var c_cvesearch = "https://cve.mitre.org/cgi-bin/cvename.cgi?name=cve-";
+var u_cvesearch = "https://people.canonical.com/~ubuntu-security/cve/";
 
 // This first one is for the header/summary of the case
 // modify font size and colors however you need for your preferences
@@ -27,16 +34,18 @@ for (var i = 0, l = els1.length; i < l; i++) {
 
   // This will hyperlink the keywords off to Google searches for those words
   // Feel free to build up a dict of any you wish to search for, samples below
-  // Note: leading spaces in keywords prevent breaking URL linking from the above construct
-  el1.innerHTML = el1.innerHTML.replace(/(openstack|Liberty|Mitaka|Newton|Queens|Pike|github|chef|preseed)/gi, '<a style="color:blue;" href="http://www.google.com/search?q=$&" title="Search Google for $&..." target="_blank">$&</a>');
+  // el1.innerHTML = el1.innerHTML.replace(/(openstack|Liberty|Mitaka|Newton|Queens|Pike|github|chef|preseed)/gi, '<a style="color:blue;" href="' + goog_search + '$&' + " title="Search Google for $&..." target="_blank">$&</a>');
+
+  // These will dynamically link in any references to CVEs to their requisite search URLs
+  // el1.innerHTML = el1.innerHTML.replace(/cve-(\d{4})-(\d{4})/gi, '<span title="Search for CVE-$1"><a style="color:blue;" href="' + c_cvesearch + '$1-$2" target="_blank">CVE-$1-$2</a></span>');
+  el1.innerHTML = el1.innerHTML.replace(/cve-(\d{4})-(\d{4})/gi, '<span title="Search for CVE-$1-$2"><a style="color:blue;" href="' + u_cvesearch + '$1/CVE-$1-$2.html" target="_blank">CVE-$1-$2</a></span>');
 
   // Attempt to turn anything that looks like a URL in a case comment, into a clickable link
-  el1.innerHTML = el1.innerHTML.replace(/(?=(https?:\/{2}[.a-z0-9A-Z%=\~\+\/&\:\?\-#]+))\1(?!['"]|<\/a>)+/gi, '<a style="color:blue;" href="$&">$&</a>');
-
+  // el1.innerHTML = el1.innerHTML.replace(/(?=(https?:\/{2}[.a-z0-9A-Z%=\~\+\/&\:\?\-#]+))\1(?!['"]|<\/a>)+/gi, '<a style="color:blue;" href="$&">$&</a>');
 }
 
-var els3 = document.querySelectorAll('.noStandardTab .dataRow.even');
-for (var k = 0, n = els3.length; k < n; k++) {
-  var el3 = els3[k];
-  el3.innerHTML = el3.innerHTML.replace(/<td class="\s+dataCell\s+"/gi, '<td class=" dataCell " style="border:1px solid #cecece; background-color: #f0f0f5;"');
+var els2 = document.querySelectorAll('.noStandardTab .dataRow.even');
+for (var k = 0, n = els2.length; k < n; k++) {
+  var el2 = els2[k];
+  el2.innerHTML = el2.innerHTML.replace(/<td class="\s+dataCell\s+"/gi, '<td class=" dataCell " style="border:1px solid #cecece; background-color: #f0f0f5;"');
 }
