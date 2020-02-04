e2f814698e57b90e2c99ddc3a3f096b47060f2b5
David A. Desrosiers <setuid@gmail.com>
2020-02-04 17:17:24 -0500

    Added support for overlying new options to the Related Lists section and Toolbox, starting with 'Log a Call' option

--- salesforce-useful-tweaks.js
+++ salesforce-useful-tweaks.js
@@ -6,8 +6,7 @@
 // @author         setuid@gmail.com
 // @updateUrl      https://raw.githubusercontent.com/desrod/browser-scripts-misc/master/salesforce-useful-tweaks.js
 // @downloadUrl    https://raw.githubusercontent.com/desrod/browser-scripts-misc/master/salesforce-useful-tweaks.js
-// @version        2.20
-// @require        https://code.jquery.com/jquery-3.4.1.js
+// @version        2.22
 // @grant          GM_addStyle
 // ==/UserScript==
 
@@ -18,10 +17,11 @@ function getElementByXpath(path) {
 	else return "";
 }
 
-var c_cvesearch = "https://cve.mitre.org/cgi-bin/cvename.cgi?name=cve-";
-var u_cvesearch = "https://people.canonical.com/~ubuntu-security/cve/";
-var attachments = getElementByXpath("/html/body//a[contains(text(),'Files')]/@href")
-var style = document.createElement('style');
+var c_cvesearch   = "https://cve.mitre.org/cgi-bin/cvename.cgi?name=cve-";
+var u_cvesearch   = "https://people.canonical.com/~ubuntu-security/cve/";
+var attachments   = getElementByXpath("/html/body//a[contains(text(),'Files')]/@href");
+var case_comments = getElementByXpath("//*[contains(text(),'Case Comments')]").replace(/.*?\((\d+)\).*/, '$1')
+var style         = document.createElement('style');
 
 // Hacky, but checks for CVE references in the case summary, re-links them as below
 document.querySelectorAll('#cas15_ileinner').forEach(node => {
@@ -42,6 +42,7 @@ document.querySelectorAll('.noStandardTab .dataRow').forEach(node => {
     // Special handling for attachments in case comments
 	node.innerHTML = node.innerHTML.replace(/\-New Attachment added: ([^()]+)/gi,
 		'&#128206; <span style="color:red;">IMPORTANT New Attachment added</span>: <a href="' + attachments + '">$1</a>')
+
 
     // Attempt to turn anything that looks like a URL in a case comment, into a clickable link
     node.innerHTML = node.innerHTML.replace(/(?=(https?:\/{2}[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&\;//=]*)?))\1(?!['"]|<\/a>)+/gim,
@@ -90,12 +91,14 @@ if (acct_dse) {
 	toolbox += `DSE: <strong>` + acct_dse.trim() + `</strong>`
 }
 
+toolbox += '<br />Case comments: <strong>' + case_comments + '</strong> total'
+
 style.innerHTML += `
 #private{background-color:#fff2e6;}
 #tam{background-color:#f1f1f1;border:1px solid #d3d3d3;border-radius:0 0 10px 10px;position:fixed;text-align:center;z-index:9;}
 #tbox_header{color:#fff;cursor:move;z-index:10;}
 #toolbox{-moz-column-width:160px;column-width:160px;font-weight:400 0;margin:1em;text-align:left;}
-.close{cursor:pointer;position:absolute;right:1%;top:10%;transform:translate(0%,-50%);}
+.close{cursor:pointer;position:absolute;right:1%;top:4%;transform:translate(0%,-50%);}
 .noStandardTab td.dataCell{font:8pt monospace!important;word-wrap:break-word;}
 .noStandardTab tr.dataRow.even td.dataCell:nth-of-type(2){background:#f0f0f5;border:1px solid #cecece;}
 div.listRelatedObject.caseBlock div.bPageBlock.brandSecondaryBrd.secondaryPalette table.list tr.even {background: #f0f0f0;}
@@ -104,6 +107,7 @@ div.listRelatedObject.caseBlock div.bPageBlock.brandSecondaryBrd.secondaryPalett
 .urgent{animation:urgent .7s infinite;}
 .watermark{color:red;font-size:1em;left:1.2em;opacity:0.5;position:absolute;vertical-align:bottom;z-index:1000;}
 div #cas15_ileinner{background-color:#90EE90;border:1px solid #cecece;color:#000;font:8pt monospace !important;padding:1em;}
+hr {border: 0; height: 1px; background-image: linear-gradient(to right, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.75), rgba(0, 0, 0, 0));}
 @keyframes urgent{
   0%{color:#f00;}
  49%{color:transparent;}
@@ -113,11 +117,19 @@ div #cas15_ileinner{background-color:#90EE90;border:1px solid #cecece;color:#000
 }`;
 document.head.appendChild(style);
 
+var log_call = document.querySelector('input[value="Log a Call"]').getAttribute('onclick')
+var m = log_call.match(/navigateToUrl(.*?['"]([^'"]*)['"])/);
+
+var related_lists = document.querySelectorAll('.efdvJumpLinkBody > ul')
+related_lists[0].insertAdjacentHTML('beforeend', '<hr /><li><a href="https://' + document.domain + m[2] + '">Log a Call</a></li>');
+
+toolbox += '<br /><br />&#9742;&nbsp;<a style="display:inline;margin:0; padding:0;" href="https://'+document.domain+m[2]+'">Log a Call</a>'
+
 var techops_toolbox = (`
  <div id="tam">
-    <div id="tbox_header">` + me + `'s Toolbox (drag)<span id="close" class="close">✖</span></div>
-    <p id="toolbox">` + toolbox + `</p>
-    </div>
+   <div id="tbox_header">` + me + `'s Toolbox (drag)<span id="close" class="close">✖</span></div>
+     <p id="toolbox">` + toolbox + `</p>
+   </div>
  </div>
 `);
 
