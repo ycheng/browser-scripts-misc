d8a428d028d33490e53de0cb9a13d8c9c7556339
David A. Desrosiers <setuid@gmail.com>
2020-02-04 17:55:07 -0500

    Removed unnecessary Case Comment cruft, added support for Time Cards

--- salesforce-useful-tweaks.js
+++ salesforce-useful-tweaks.js
@@ -6,7 +6,7 @@
 // @author         setuid@gmail.com
 // @updateUrl      https://raw.githubusercontent.com/desrod/browser-scripts-misc/master/salesforce-useful-tweaks.js
 // @downloadUrl    https://raw.githubusercontent.com/desrod/browser-scripts-misc/master/salesforce-useful-tweaks.js
-// @version        2.22
+// @version        2.23
 // @grant          GM_addStyle
 // ==/UserScript==
 
@@ -17,17 +17,20 @@ function getElementByXpath(path) {
 	else return "";
 }
 
-var c_cvesearch   = "https://cve.mitre.org/cgi-bin/cvename.cgi?name=cve-";
-var u_cvesearch   = "https://people.canonical.com/~ubuntu-security/cve/";
-var attachments   = getElementByXpath("/html/body//a[contains(text(),'Files')]/@href");
-var case_comments = getElementByXpath("//*[contains(text(),'Case Comments')]").replace(/.*?\((\d+)\).*/, '$1')
-var style         = document.createElement('style');
+var c_cvesearch = "https://cve.mitre.org/cgi-bin/cvename.cgi?name=cve-";
+var u_cvesearch = "https://people.canonical.com/~ubuntu-security/cve/";
+var attachments = getElementByXpath("/html/body//a[contains(text(),'Files')]/@href");
+var style = document.createElement('style');
 
 // Hacky, but checks for CVE references in the case summary, re-links them as below
 document.querySelectorAll('#cas15_ileinner').forEach(node => {
 	node.innerHTML = node.innerHTML.replace(/(?:[^\/])(cve-\d{4}-\d{4,7})/gim,
 		'<span title="Search for $1">&nbsp;<a style="color:blue;" href="' + u_cvesearch + '$1.html" target="_blank">$1</a></span>')
 });
+
+// document.getElementByXpath("/html/body//td[contains(text(),'files.support')]").forEach(node => {
+// 	 alert(node)
+// });
 
 var me = document.getElementsByTagName('span')
 me = me[1].title.replace(/(\w+)\s.*/, '$1')
@@ -43,7 +46,6 @@ document.querySelectorAll('.noStandardTab .dataRow').forEach(node => {
 	node.innerHTML = node.innerHTML.replace(/\-New Attachment added: ([^()]+)/gi,
 		'&#128206; <span style="color:red;">IMPORTANT New Attachment added</span>: <a href="' + attachments + '">$1</a>')
 
-
     // Attempt to turn anything that looks like a URL in a case comment, into a clickable link
     node.innerHTML = node.innerHTML.replace(/(?=(https?:\/{2}[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&\;//=]*)?))\1(?!['"]|<\/a>)+/gim,
 		'<a style="color:blue;" href="$&">$&</a>')
@@ -58,6 +60,7 @@ document.querySelectorAll('.noStandardTab .dataRow').forEach(node => {
 	// I don't like it, I'll fix it later.
 	node.innerHTML = node.innerHTML.replace(/<a href(.*) title="Make Public(.*?)<td class="\s+dataCell\s+">(.*)/gi,
 		'<a href $1 title="Make Public $2<td class="dataCell" id="private"><span class="watermark">private comment</span>$3')
+
 });
 
 var toolbox = ''
@@ -91,8 +94,6 @@ if (acct_dse) {
 	toolbox += `DSE: <strong>` + acct_dse.trim() + `</strong>`
 }
 
-toolbox += '<br />Case comments: <strong>' + case_comments + '</strong> total'
-
 style.innerHTML += `
 #private{background-color:#fff2e6;}
 #tam{background-color:#f1f1f1;border:1px solid #d3d3d3;border-radius:0 0 10px 10px;position:fixed;text-align:center;z-index:9;}
@@ -118,12 +119,16 @@ hr {border: 0; height: 1px; background-image: linear-gradient(to right, rgba(0, 
 document.head.appendChild(style);
 
 var log_call = document.querySelector('input[value="Log a Call"]').getAttribute('onclick')
-var m = log_call.match(/navigateToUrl(.*?['"]([^'"]*)['"])/);
+var log_call_m = log_call.match(/navigateToUrl(.*?['"]([^'"]*)['"])/);
+
+var new_timecard = document.querySelector('input[value="New time card"]').getAttribute('onclick')
+var new_timecard_m = new_timecard.match(/this.form.action = (.*?['"]([^'"]*)['"])/);
 
 var related_lists = document.querySelectorAll('.efdvJumpLinkBody > ul')
-related_lists[0].insertAdjacentHTML('beforeend', '<hr /><li><a href="https://' + document.domain + m[2] + '">Log a Call</a></li>');
+related_lists[0].insertAdjacentHTML('beforeend', '<hr /><li><a href="https://' + document.domain + log_call_m[2] + '">Log a Call</a></li>');
 
-toolbox += '<br /><br />&#9742;&nbsp;<a style="display:inline;margin:0; padding:0;" href="https://'+document.domain+m[2]+'">Log a Call</a>'
+toolbox += '<hr />&#9201;&nbsp;<a style="display:inline;margin:0; padding:0;" href="https://' + document.domain + new_timecard_m[2] + '">New time card</a>'
+toolbox += '<br />&#9742;&nbsp;<a style="display:inline;margin:0; padding:0;" href="https://' + document.domain + log_call_m[2] + '">Log a Call</a></span>'
 
 var techops_toolbox = (`
  <div id="tam">
@@ -135,6 +140,7 @@ var techops_toolbox = (`
 
 var append_toolbox = document.getElementsByClassName('efdvJumpLinkBody')
 append_toolbox[0].innerHTML += techops_toolbox
+
 
 // This is needed to create the draggable toolbox around the page
 dragElement(document.getElementById('tam'));
