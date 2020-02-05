204120f3d2d0ab9b8663c3f5720382bb9fc312e0
David A. Desrosiers <setuid@gmail.com>
2020-02-05 18:36:05 -0500

    Cleaning up the use of variables, similar to Python's f-strings, cleaning up attachments in preparation for next steps.

--- salesforce-useful-tweaks.js
+++ salesforce-useful-tweaks.js
@@ -6,7 +6,7 @@
 // @author         setuid@gmail.com
 // @updateUrl      https://raw.githubusercontent.com/desrod/browser-scripts-misc/master/salesforce-useful-tweaks.js
 // @downloadUrl    https://raw.githubusercontent.com/desrod/browser-scripts-misc/master/salesforce-useful-tweaks.js
-// @version        2.32
+// @version        2.34
 // @grant          GM_addStyle
 // ==/UserScript==
 
@@ -31,22 +31,22 @@ var sev_level = getElementByXpath("//*[contains(text(),'Severity Level')]/follow
 
 var customer = getElementByXpath("//*[contains(text(),'Customer')]/following::a[2]")
 if (customer) {
-	toolbox += `Customer user: <strong>` + customer.trim() + `</strong><br />`
+	toolbox += `Customer user: <strong>${customer.trim()}</strong><br />`
 }
 
 var case_owner = getElementByXpath("//*[contains(text(),'Case Owner')]/following::td[1]")
 if (case_owner) {
-	toolbox += `Case owner: <strong>` + case_owner.trim() + `</strong><br />`
+	toolbox += `Case owner: <strong>${case_owner.trim()}</strong><br />`
 }
 
 var acct_tam = getElementByXpath("//*[contains(text(),'Technical Account Manager')]/preceding::th[1]").replace(/User:(.*?)/, '$1')
 if (acct_tam) {
-	toolbox += `TAM: <strong>` + acct_tam.trim() + `</strong><br />`
+	toolbox += `TAM: <strong>${acct_tam.trim()}</strong><br />`
 }
 
 var acct_dse = getElementByXpath("//*[contains(text(),'Dedicated')]/preceding::th[1]").replace(/User:(.*?)/, '$1')
 if (acct_dse) {
-	toolbox += `DSE: <strong>` + acct_dse.trim() + `</strong>`
+	toolbox += `DSE: <strong>${acct_dse.trim()}</strong>`
 }
 
 // Query selectors by XPath
@@ -62,11 +62,10 @@ document.querySelectorAll('#cas15_ileinner').forEach(node => {
 		'<span title="Search for $1">&nbsp;<a style="color:blue;" href="' + u_cvesearch + '$1.html" target="_blank">$1</a></span>')
 });
 
-
 document.querySelectorAll('.noStandardTab .dataRow').forEach(node => {
     // Build an array of all attachments linked in the case comments
     if (node.innerHTML.match(/<br>https:\/\/files.support.*/)) {
-        case_attachments.push(node.innerHTML.match(/https?:\/{2}[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&\;//=]*)?/gi))
+        case_attachments.push(node.innerHTML.match(/https?:\/([-a-zA-Z0-9()@:%_\+.~#?&\;//=]*)?/gi))
     }
     // alert(node.innerHTML)
     node.innerHTML = node.innerHTML.replace(/(Created By:.*)/,
@@ -77,7 +76,7 @@ document.querySelectorAll('.noStandardTab .dataRow').forEach(node => {
 
     // Special handling for attachments in case comments
 	node.innerHTML = node.innerHTML.replace(/\-New Attachment added: ([^()]+)/gi,
-		'&#128206; <span style="color:red;">IMPORTANT New Attachment added</span>: <a href="' + attachments + '">$1</a>')
+		`&#128206; <span style="color:red;">IMPORTANT New Attachment added</span>: <a href="${attachments}">$1</a>`)
 
     // Attempt to turn anything that looks like a URL in a case comment, into a clickable link
     node.innerHTML = node.innerHTML.replace(/(?=(https?:\/{2}[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&\;//=]*)?))\1(?!['"]|<\/a>)+/gim,
@@ -128,10 +127,10 @@ hr {border: 0; height: 1px; background-image: linear-gradient(to right, rgba(0, 
 
 if (sev_level) {
 	// Add some urgency to the L1 level cases
-	sev_level.includes('L1') ? sev_level = `<span class="urgent">` + sev_level + `</span>` : sev_level
+	sev_level.includes('L1') ? sev_level = `<span class="urgent">${sev_level}</span>` : sev_level
 	sev_level.includes('L1') ? tbox_header = '#f00' : tbox_header = '#4287f5'
     sev_level.includes('L1') ? style.innerHTML += '.efdvJumpLink{border:2px solid #f00;border-radius:10px;}' : ''
-	toolbox += `Severity: <strong>` + sev_level.trim() + `</strong><br />`
+	toolbox += `Severity: <strong>${sev_level.trim()}</strong><br />`
 }
 
 if (document.getElementsByClassName('efdvJumpLinkBody').length > 0) {
@@ -148,14 +147,14 @@ if (document.getElementsByClassName('efdvJumpLinkBody').length > 0) {
     related_list_items[0].insertAdjacentHTML('beforeend', '<hr />Uploaded files:')
     var index = 0
     while (index < case_attachments.length) {
-        related_list_items[0].insertAdjacentHTML('beforeend', `<li style="overflow-wrap: break-word;"><a href="${case_attachments[index]}">&#128193; File ${case_attachments[index][0].split('/').slice(-1)[0]} (${index})</a></li>`);
-        // related_list_items[0].insertAdjacentHTML('beforeend', `<li><a href="${case_attachments[index]}" title="${case_attachments[index][0].split('/').slice(-1)[0]}">&#128193; File (${index})</a></li>`);
+        related_list_items[0].insertAdjacentHTML('beforeend',
+                                                 `<li style="overflow-wrap: break-word;"><a href="${case_attachments[index]}" target="_blank">&#128193; File ${case_attachments[index][0].split('/').slice(-1)[0]} (${index})</a></li>`);
         index++
     }
-    related_list_items[0].insertAdjacentHTML('beforeend', '<hr /><li><a class="tbox_call" title="All calls must be logged separately from time cards" href="https://' + log_call_msg + '">Log a Call</a></li>');
-    related_list_items[0].insertAdjacentHTML('beforeend', '<li><a title="Add a new time card. Must be done by EOD!" class="tbox_time" href="https://' + new_timecard_msg + '">New time card</a></li>');
+    related_list_items[0].insertAdjacentHTML('beforeend', `<hr /><li><a class="tbox_call" title="All calls must be logged separately from time cards" href="https://${log_call_msg}" target="_blank">Log a Call</a></li>`);
+    related_list_items[0].insertAdjacentHTML('beforeend', `<li><a title="Add a new time card. Must be done by EOD!" class="tbox_time" href="https://${new_timecard_msg}" target="_blank">New time card</a></li>`);
     append_toolbox = document.getElementsByClassName('efdvJumpLinkBody')
-    related_list_items[0].insertAdjacentHTML('beforebegin', '<br />' + toolbox + '<hr />')
+    related_list_items[0].insertAdjacentHTML('beforebegin', `<br />${toolbox}<hr />`)
 } else { // Non-case-related page rendering
     append_toolbox = document.getElementsByClassName('thumbnailTable')
     style.innerHTML += `#tools{border:1px solid #ccc;}#toolbox{-moz-column-width:200px;column-width:200px;}`
